59502d79960c9aff63496729ccda4bc55748be3f
Paweł Benkowski <pawlo97.pb@gmail.com>
2020-03-11 20:23:18 +0100

    AWS is a fucking piece of shit

--- dennis-crash-course/crm/crm/settings.py
+++ dennis-crash-course/crm/crm/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = "crm.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "DEMO_TEST",
+        "USER": "postgres",
+        "PASSWORD": "51184c6a",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
