fe8359f5111df8db49847033ad0640615f5d1bc0
Paweł Benkowski <pawlo97.pb@gmail.com>
2020-03-15 17:33:26 +0100

    Part 7

--- docs_app/polls/admin.py
+++ docs_app/polls/admin.py
@@ -4,5 +4,28 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# admin.site.register(Choice)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "question_text"]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    fieldsets = (
+        (None, {"fields": ("question_text",),}),
+        (
+            "Date information",
+            {"fields": ("pub_date",), "classes": ("collapse",)},
+        ),
+    )
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
