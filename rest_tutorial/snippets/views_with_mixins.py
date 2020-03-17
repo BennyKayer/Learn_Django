353b9a30107bd146ecaf4968a4b4155b4e99fbb7
Paweł Benkowski <pawlo97.pb@gmail.com>
2020-03-17 19:10:37 +0100

    Part 3

---
+++ rest_tutorial/snippets/views_with_mixins.py
@@ -0,0 +1,37 @@
+from rest_framework import mixins
+from rest_framework import generics
+
+from .models import Snippet
+from .serializers import SnippetSerializer
+
+
+class SnippetList(
+    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
+):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class SnippetDetail(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
